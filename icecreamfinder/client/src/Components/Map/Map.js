c3f7160e5b150c0b36b0d1b3427f9f80c8b0e61d
Philip Vieira <philipvieira00@gmail.com>
2019-10-15 21:33:56 -0400

    I'm not missing the rest of my changes

--- icecreamfinder/client/src/Components/Map/Map.js
+++ icecreamfinder/client/src/Components/Map/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+require("dotenv").config();
  
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
  
