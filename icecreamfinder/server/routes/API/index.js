64120d57e1256666cf889a8ae970a7e0e4ea2fcf
ryanlester91 <50120534+ryanlester91@users.noreply.github.com>
2019-10-17 22:37:52 -0400

    Update index.js

--- icecreamfinder/server/routes/API/index.js
+++ icecreamfinder/server/routes/API/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const truckRoutes = require("../../models/Truck");
 const productRoutes = require("../../models/Products");
-const userRoutes = require("../../models/User");
+//const userRoutes = require("../../models/User");
 
 router.use("/products", productRoutes);
 router.use("/trucks", truckRoutes);
-router.use("/users", userRoutes);
+//router.use("/users", userRoutes);
 
 module.exports = router;
