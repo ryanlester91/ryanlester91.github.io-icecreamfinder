0a65936a3bd37a87e13b8f8d73b109e96e380275
ryanlester91 <50120534+ryanlester91@users.noreply.github.com>
2019-10-17 22:36:05 -0400

    Removing passport stuff from server page

--- icecreamfinder/server.js
+++ icecreamfinder/server.js
@@ -1,23 +1,14 @@
-// //currently:
-// //defaults to login page with option to click link to redirect to register page 
-// //redirects from register to login pages but the login page redirects to itself-- failureRedirect
-// if (process.env.NODE_ENV !=="production") {
-// 	require("dotenv").config();
-// }
+
 require('dotenv').config();
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt");
-const passport = require("passport");
-const flash  = require("express-flash");
-const session = require("express-session");
-const methodOverride = require("method-override");
+
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 
 const routes = require("./server/routes");
-//const MongoStore = require("connect-mongostore")(session);
+
 
 //connecting to mongoDB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/usericecreamtruck");
@@ -36,35 +27,15 @@ db.once("open", function(){
 
 
 
-const initializePassport = require("./passport-config")
-initializePassport(
-	passport, 
-	//name => users.find(user => user.name === name),
-	email => users.find(user => user.email === email),
-	id => users.find(user => user.id === id)
-)
 
-app.set("view-engine", "ejs");
-//app.set("trust proxy", 1);
+app.set("view-engine", "html");
+
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false}))
 app.use(express.json());
-app.use(flash());
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	resave: false,
-	saveUninitialized: false,
-	cookie: { secure: true },
-	//store: new MongoStore({
-		//mongooseConnection: db
-	//})
-}));
 
-const users=[] //just for the sake of practicing, inside app.post /register
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(methodOverride("_method"));
+
 
 
 
@@ -74,101 +45,14 @@ if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
   }
   
-  // Send every request to the React app
-  // Define any API routes before this runs
-  /*app.get("*", function(req, res) {
-	res.sendFile(path.join(__dirname, "./client/build"));
-  });*/
+  
 
 
 
-//routes
-//app(routes);
-//let User = require("./server/models/User");
-//let User = require("./server/models/API/User");
+
 app.use(routes);
 
-app.get("/", checkAuthenticated, (req, res) => {
-	res.render("index.ejs", { email: req.user.email })
-});
 
-app.get("/:email/:password", function(req, res) {
-	let newUser = new User();
-	newUser.local.email = req.params.email;
-	newUser.local.password = req.params.password;
-	console.log(newUser.local.username);
-	console.log(newUser.local.password);
-	newUser.save(function(err) {
-		if(err) {
-			throw err;
-		}
-	});
-	res.send("Success!");
-})
-
-app.get("/login", checkNotAuthenticated, (req, res) => {
-	res.render("login.ejs")
-});
-app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
-	successRedirect: "/",
-	failureRedirect: "/login",
-	failureFlash: true
-}));
-
-//app.post("/login", (req, res) => {
-	//res.redirect("/");
-//}); 
-
-app.get("/register", checkNotAuthenticated, (req, res) => {
-	res.render("register.ejs")
-});
-
-app.post("/register", checkNotAuthenticated, async (req, res) => {
-	var password = req.body.password;
-
-	var newUser = new User({
-		email: req.body.email,
-		password: req.body.password		
-	});
-	/*User.createUser(newUser, function(err, user) {
-		if (err) throw (err);
-		res.send(user).end();*/
-	//})
-	try {
-		const hashedPassword = await bcrypt.hash(req.body.password, 10)
-		users.push({
-		id: Date.now().toString(),
-		//name: req.body.name,
-		email: req.body.email,
-		password: hashedPassword 
-	})
-	*res.redirect("/login")
-	 } catch {
-	res.redirect("/register")	
-	}
-	console.log(users);
-});
-
-app.delete("/logout", (req, res) => {
-	req.logOut(); //function automatically added by Passport program
-	res.redirect("/login");
-})
-
-function checkAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) {
-		return next();
-	}
-	res.redirect("/login");
-}
-
-
-function checkNotAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) {
-		return res.redirect('/');
-	}
-	next();
-
-}
 app.listen(3000);
 
 
